e8a967dcec619e8e755d28ee8a747d411ec14e55
patruong <ptruong@kth.se>
2023-07-13 17:26:58 +0200

    update

--- scripts/feature_importance_plot_all_samples.py
+++ scripts/feature_importance_plot_all_samples.py
@@ -11,37 +11,102 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import make_regression
+import xgboost as xgb
+import lightgbm as lgb
 
-def plot_feature_importance(df, title="Feature importances", subplot_index=111):
-    feature_names = df.drop("log10_lu_ratio", axis=1).columns
+def plot_feature_importance(df, title="Feature importances", subplot_index=111, peptide_sequence_encoding=False, model = "randomForest"):
 
-    X = df.drop("log10_lu_ratio", axis=1).values
-    y = df["log10_lu_ratio"].values
+    if type(peptide_sequence_encoding) == pd.core.frame.DataFrame:
+        peptide_sequence_encoding = peptide_sequence_encoding.reset_index().drop("index", axis = 1)
+        X = df.drop(["log10_lu_ratio", "peptide"], axis=1)#.values
+        X = X.reset_index().drop("index", axis = 1)
+        X = pd.concat([X, peptide_sequence_encoding], axis = 1)
+        feature_names = X.columns
+        X = X.values
+        y = df["log10_lu_ratio"].values
+    else:
+        X = df.drop(["log10_lu_ratio", "peptide"], axis=1).values
+        feature_names = df.drop("log10_lu_ratio", axis=1).columns
+        y = df["log10_lu_ratio"].values
 
-    # Create a random forest regressor
-    forest = RandomForestRegressor(n_estimators=100, random_state=42)
 
-    # Fit the random forest regressor
-    forest.fit(X, y)
+    # RANDOM FOREST
+    if model == "randomForest":
+        # Create a random forest regressor
+        forest = RandomForestRegressor(n_estimators=100, random_state=42)
+    
+        # Fit the random forest regressor
+        forest.fit(X, y)
+    
+        # Calculate feature importances
+        importances = forest.feature_importances_
+        std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+        indices = np.argsort(importances)[::-1]
+        
+        # Print the feature ranking
+        print(f"Feature ranking ({title}):")
+        for f in range(X.shape[1]):
+            print(f"{f + 1}. {feature_names[indices[f]]} ({importances[indices[f]]})")
 
-    # Calculate feature importances
-    importances = forest.feature_importances_
-    std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
-    indices = np.argsort(importances)[::-1]
+        # Plot the feature importances
+        ax = plt.subplot(subplot_index)
+        ax.bar(range(X.shape[1]), importances[indices], color="r", yerr=std[indices], align="center")
+        ax.set_title(title)
+        ax.set_xticks(range(X.shape[1]))
+        ax.set_xticklabels([feature_names[i] for i in indices], rotation='vertical')
+        ax.set_xlim([-1, X.shape[1]])
 
-    # Print the feature ranking
-    print(f"Feature ranking ({title}):")
-    for f in range(X.shape[1]):
-        print(f"{f + 1}. {feature_names[indices[f]]} ({importances[indices[f]]})")
 
-    # Plot the feature importances
-    ax = plt.subplot(subplot_index)
-    ax.bar(range(X.shape[1]), importances[indices], color="r", yerr=std[indices], align="center")
-    ax.set_title(title)
-    ax.set_xticks(range(X.shape[1]))
-    ax.set_xticklabels([feature_names[i] for i in indices], rotation='vertical')
-    ax.set_xlim([-1, X.shape[1]])
+    if model == "xgboost":
+        # Train the XGBoost regression model
+        model = xgb.XGBRegressor(n_estimators=100)
+        model.fit(X, y)
 
+        # Get feature importances
+        importances = model.feature_importances_
+
+        indices = np.argsort(importances)[::-1]
+        
+        # Print the feature ranking
+        print(f"Feature ranking ({title}):")
+        for f in range(X.shape[1]):
+            print(f"{f + 1}. {feature_names[indices[f]]} ({importances[indices[f]]})")
+
+        # Plot the feature importances
+        ax = plt.subplot(subplot_index)
+        ax.bar(range(X.shape[1]), importances[indices], color="r", align="center")
+        ax.set_title(title)
+        ax.set_xticks(range(X.shape[1]))
+        ax.set_xticklabels([feature_names[i] for i in indices], rotation='vertical')
+        ax.set_xlim([-1, X.shape[1]])
+        
+    if model == "lightgbm":
+        # Train the LightGBM regression model
+        model = lgb.LGBMRegressor(n_estimators=100)
+        model.fit(X, y)
+        
+        # Get feature importances
+        importances = model.feature_importances_
+        indices = np.argsort(importances)[::-1]
+        
+        # Print the feature ranking
+        print(f"Feature ranking ({title}):")
+        for f in range(X.shape[1]):
+            print(f"{f + 1}. {feature_names[indices[f]]} ({importances[indices[f]]})")
+
+        # Plot the feature importances
+        ax = plt.subplot(subplot_index)
+        ax.bar(range(X.shape[1]), importances[indices], color="r", align="center")
+        ax.set_title(title)
+        ax.set_xticks(range(X.shape[1]))
+        ax.set_xticklabels([feature_names[i] for i in indices], rotation='vertical')
+        ax.set_xlim([-1, X.shape[1]])
+
+
+
+# Print feature importance scores
+for feature, importance in zip(features.columns, importances):
+    print(f"{feature}: {importance}")
 
 # Create a 2x3 grid for subplots
 fig, axs = plt.subplots(1, 1, figsize=(12, 8))
@@ -55,9 +120,6 @@ for i, dataset in enumerate(datasets):
     dfs.append(df)
 
 df = pd.concat(dfs)
-
-
-
 
 
 # one-hot-encoded-sequences.
@@ -80,19 +142,51 @@ by_count = one_hot_encoding_count(df)
 by_position = one_hot_encoding_positional(df)
 one_hot_encoded_sequences = one_hot_encoding_aminoacid(df)
 
-features = pd.concat([one_hot_encoded_sequences, df[["feature1", "feature2"]]], axis=1)
-
-
-
 
 
 ####
 
-plot_feature_importance(df, title="All C", subplot_index=111)
-
+plot_feature_importance(df, title="All C", subplot_index=111, peptide_sequence_encoding=False)
 # Adjust the spacing between subplots
 plt.tight_layout()
-
 # Show the plot
 plt.show()
+
+# peptide_sequence by count
+plot_feature_importance(df, title="All C", subplot_index=111, peptide_sequence_encoding=by_count)
+# Adjust the spacing between subplots
+plt.tight_layout()
+# Show the plot
+plt.show()
+
+# peptide sequence by aminoacid
+plot_feature_importance(df, title="All C", subplot_index=111, peptide_sequence_encoding=one_hot_encoded_sequences)
+# Adjust the spacing between subplots
+plt.tight_layout()
+# Show the plot
+plt.show()
+
+# peptide sequence by position
+plot_feature_importance(df, title="All C", subplot_index=111, peptide_sequence_encoding=by_position)
+# Adjust the spacing between subplots
+plt.tight_layout()
+# Show the plot
+plt.show()
+
+# peptide sequence by position
+plot_feature_importance(df, title="All C", subplot_index=111, peptide_sequence_encoding=False, model = "xgboost")
+# Adjust the spacing between subplots
+plt.tight_layout()
+# Show the plot
+plt.show()
+
+
+# peptide sequence by position
+plot_feature_importance(df, title="All C", subplot_index=111, peptide_sequence_encoding=False, model = "lightgbm")
+# Adjust the spacing between subplots
+plt.tight_layout()
+# Show the plot
+plt.show()
+
+
 
